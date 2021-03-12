635cc28b9d77e1ffa0c8ada16bb7c8b0d523cbfd
wezley <wezley.singleton@gmail.com>
2021-03-12 16:10:53 -0500

    added a fetch call to demo that it can grab data from elsewhere

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-window.onload = function() {
-    console.log('Hello world!')
+window.onload = async () => {
+	let response = await fetch('http://quizzardapi-env-1.eba-3m7rumy3.us-east-1.elasticbeanstalk.com/questions');
 }
