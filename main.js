48d0f65fd467a6c16b036c8da3cdcf5b96696d60
wezley <wezley.singleton@gmail.com>
2021-03-12 16:15:08 -0500

    dumber

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.onload = async () => {
 	let response = await fetch('http://quizzardapi-env-1.eba-3m7rumy3.us-east-1.elasticbeanstalk.com/questions');
-	console.log(response);
+	let questions = await response.json();
+	console.log(questions);
 }
